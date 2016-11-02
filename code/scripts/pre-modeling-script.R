cd8aa2fe135e20b088f9d404751e8a10bfc940e9
Shirley Jin <x.jin@berkeley.edu>
2016-11-02 01:07:15 -0700

    scripts: new pre-modelling processing R script

---
+++ code/scripts/pre-modeling-script.R
@@ -0,0 +1,15 @@
+# loading data
+credit <- read.csv("../../data/Credit.csv")
+credit <- credit[,-1]
+
+# dummy out categorical variables
+temp_credit <- model.matrix(Balance ~ ., data = credit)
+
+# removing column of ones, and appending Balance
+new_credit <- cbind(temp_credit[ ,-1], Balance = credit$Balance)
+
+# scaling and centering
+scaled_credit <- scale(new_credit, center = TRUE, scale = TRUE)
+
+# export scaled data
+write.csv(scaled_credit, file = "path/of/processed/data/scaled-credit.csv")
