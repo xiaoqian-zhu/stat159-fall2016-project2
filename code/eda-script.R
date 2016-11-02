0175ca8821bc8ac72ac82f35b0b67ccb5caa0524
xiaoqian-zhu <xqzhu@berkeley.edu>
2016-11-02 01:21:43 -0700

    code: finish qualitative variables part

--- code/eda-script.R
+++ code/eda-script.R
@@ -30,10 +30,36 @@ range <- c(max(balance)-min(balance),
            max(cards)-min(cards),
            max(age)-min(age),
            max(education)-min(education))
-quan_var <- data.frame(categories, sd, iqr, range)
+quan_df <- data.frame(categories, sd, iqr, range)
 
-# Make data frames for qualitative variables
-qual_var <- data.frame(gender, student, marriage, ethnicity)
+# Qualitative Variable Information Tables
+# Gender
+gender_freq <- table(credit$Gender)
+gender_rel_freq <- gender_freq/nrow(credit)
+gender_df <- data.frame(gender_freq, gender_rel_freq)
+gender_df$Var1.1 <- NULL
+colnames(gender_df) <- c('Gender', 'Frequency', 'Relative Frequency')
+
+# Student
+student_freq <- table(credit$Student)
+student_rel_freq <- student_freq/nrow(credit)
+student_df <- data.frame(student_freq, student_rel_freq)
+student_df$Var1.1 <- NULL
+colnames(student_df) <- c('Student?', 'Frequency', 'Relative Frequency')
+
+# Married
+married_freq <- table(credit$Married)
+married_rel_freq <- married_freq/nrow(credit)
+married_df <- data.frame(married_freq, married_rel_freq)
+married_df$Var1.1 <- NULL
+colnames(married_df) <- c('Married?', 'Frequency', 'Relative Frequency')
+
+# Ethnicity
+ethnicity_freq <- table(credit$Ethnicity)
+ethnicity_rel_freq <- ethnicity_freq/nrow(credit)
+ethnicity_df <- data.frame(ethnicity_freq, ethnicity_rel_freq)
+ethnicity_df$Var1.1 <- NULL
+colnames(ethnicity_df) <- c('Ethnicity', 'Frequency', 'Relative Frequency')
 
 
 # Sink to Eda Output file
@@ -54,7 +80,22 @@ summary(credit$Age)
 cat('\nSummary of Education\n')
 summary(credit$Education)
 cat('\nSummary of Other Descriptive Statistics\n')
-print(qual_var)
+print(quan_df)
+cat('\n\nQualitative Variable Tables\n')
+cat('\nGender Table\n')
+print(gender_df)
+cat('\nStudent Table\n')
+print(student_df)
+cat('\nMarried Table\n')
+print(married_df)
+cat('\nEthnicity Table\n')
+print(ethnicity_df)
+cat('\n\nRelationship Between Balance and Qualitative Variables\n')
+cat('\nAnova\n')
+aov(Balance ~ Gender + Student + Married + Ethnicity, data = credit)
+sink()
+
+
 
 # Histograms for Quantitative Variables
 png('images/histogram-balance.png')
@@ -149,12 +190,64 @@ png('images/boxplot-education.png')
 boxplot(education, ylab = 'Frequency', main = 'Boxplot of Education')
 dev.off()
 
+# Barchats for Qualitative Variables
+png('images/barchart-gender.png')
+barplot(table(credit$Gender), ylab = 'Count', xlab = 'Gender',
+        col = 'red')
+dev.off()
+
+png('images/barchart-student.png')
+barplot(table(credit$Student), ylab = 'Count', xlab = 'Student?',
+        col = 'red')
+dev.off()
+
+png('images/barchart-married.png')
+barplot(table(credit$Married), ylab = 'Count', xlab = 'Married?',
+        col = 'red')
+dev.off()
+
+png('images/barchart-ethnicity.png')
+barplot(table(credit$Ethnicity), ylab = 'Count', xlab = 'Ethnicity',
+        col = 'red', ylim = c(0, 250))
+dev.off()
+
+
 # Correlations Matrix between Quantitative Variables
-corr_matrix <- cor(quan_var)
+corr_matrix <- cor(quan_df)
+print(corr_matrix)
+correlation_matrix[lower.tri(correlation_matrix)] <- ''
+
+sink('data/eda-correlation-matrix.txt')
 print(corr_matrix)
 sink()
 
 # Scatterplot Matrix
 png('images/scatterplot-matrix.png')
-pairs(quant,pch = 16)
+pairs(quan_df,pch = 16)
+dev.off()
+
+# Boxplots relating Balance to Each of the Qualitative Variables
+png('images/boxplot-gender.png')
+ggplot(data = credit) +
+  geom_boxplot(aes(x = Gender, y = Balance), fill = c('blue','red'))+
+  ggtitle('Conditional Boxplot of Balance by Gender')
+dev.off()
+
+png('images/boxplot-student.png')
+ggplot(data = credit) +
+  geom_boxplot(aes(x = Student, y = Balance), fill = c('blue','red'))+
+  ggtitle('Conditional Boxplot of Balance by Student')
+dev.off()
+
+png('images/boxplot-married.png')
+ggplot(data = credit) +
+  geom_boxplot(aes(x = Married, y = Balance), fill = c('blue','red'))+
+  ggtitle('Conditional Boxplot of Balance by Married')
+dev.off()
+
+png('images/boxplot-ethnicity.png')
+ggplot(data = credit) +
+  geom_boxplot(aes(x = Ethnicity, y = Balance), 
+               fill = c('blue','red', 'yellow'))+
+  ggtitle('Conditional Boxplot of Balance by Ethnicity')
 dev.off()
