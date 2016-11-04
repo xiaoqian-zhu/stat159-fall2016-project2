5417fde0efe3e7a519827a511b7bc45b7bea3da0
Shirley Jin <x.jin@berkeley.edu>
2016-11-03 17:27:21 -0700

    scripts: new ols regression script

---
+++ code/scripts/ols-script.R
@@ -0,0 +1,25 @@
+# Loading dataset
+scaled_credit<- read.csv("../../data/datasets/scaled-credit.csv")
+
+# Fitting ols regression and regression summary statistics
+ols_regression <- lm(Balance ~ .,data = scaled_credit)
+ols_summary <- summary(ols_regression)
+
+# Regression coefficients
+ols_coefficients <- coef(ols_regression)
+
+# Computing MSE from ols regression
+ols_mse <- mean(ols_regression$residuals^2)
+
+# Saving output to RData file
+save(ols_regression,ols_summary,ols_coefficients, ols_mse, file="../../data/ols-regression.RData")
+
+# Sinking output to txt file
+sink('../../data/ols-regression-output.txt')
+cat('\nOLS regression summary statistics\n')
+print(ols_summary)
+cat('\nOLS MSE\n')
+print(ols_mse)
+cat('\nOLS regression coefficients\n')
+print(ols_coefficients)
+sink()
