f2f396c181451f1f371cbf8dbdd001cd1aa1d3ac
xiaoqian-zhu <xqzhu@berkeley.edu>
2016-11-04 03:04:57 -0700

    scripts: fix typos in lasso-regression.R

--- code/scripts/lasso-regression.R
+++ code/scripts/lasso-regression.R
@@ -14,7 +14,7 @@ y_test <- test_set[,12]
 x_credit <- scaled_credit[,-12]
 y_credit <- scaled_credit[,12]
 
-# Fit model on train data using ten fold validation
+# Fit model on train data using ten-fold validation
 set.seed(1)
 grid <- 10^seq(10, -2, length = 100)
 lasso_cv <- cv.glmnet(as.matrix(x_train), y_train, alpha = 1, lambda = grid, intercept = FALSE, standardize = FALSE)
@@ -22,7 +22,7 @@ lasso_cv <- cv.glmnet(as.matrix(x_train), y_train, alpha = 1, lambda = grid, int
 # Get lambda for best fit model
 lasso_min_lambda <- lasso_cv$lambda.min
 
-# Plot cross validation errors
+# Plot cross-validation errors
 png('../../images/lasso-cross-validation-errors.png')
 plot(lasso_cv)
 dev.off()
@@ -36,10 +36,10 @@ lasso_mse <- mse(lasso_predict,y_test)
 lasso_regression <- glmnet(as.matrix(x_credit), y_credit, alpha = 1, lambda = lasso_min_lambda, intercept = FALSE, standardize = FALSE)
 lasso_coefficients <- coef(lasso_regression)
 
-# Save outputs
+# Save output
 save(lasso_cv, lasso_min_lambda, lasso_mse, lasso_regression, lasso_coefficients, file = "../../data/output/lasso-regression.RData")
 
-# Sink outputs
+# Sink output
 sink('../../data/output/lasso-regression-output.txt')
 cat('\nLasso best fitting lambda\n')
 print(lasso_min_lambda)
